ad95a45495ff4923b6346b81faf06914f4228889
FatouSall <sallfatoumalle@gmail.com>
2017-05-11 13:22:45 -0400

    image a deviner et mise en forme

--- static/js/main.js
+++ static/js/main.js
@@ -4,20 +4,15 @@
 
 "use strict";
 
-document.body.style.backgroundImage="url(static/img/pik3.jpg)";
+document.body.style.backgroundImage="url(static/img/pik3.png)";
 document.body.style.backgroundSize='cover';
 document.body.style.backgroundRepeat="no-repeat";
 document.getElementById('tableau').style.display='inline-block';
 
 var div_answer_style=document.getElementById('div_answer');
 div_answer_style.style.width='300px';
-div_answer_style.style.marginTop="40px";
-div_answer_style.style.marginLeft="100px";
 
 var div_reset_style=document.getElementById('div_reset');
-div_reset_style.style.width='300px';
-div_reset_style.style.marginTop="20px";
-div_reset_style.style.marginLeft="130px";
 var labs=document.getElementsByTagName('label');
 for (var i=0;i<labs.length;i++){
   labs[i].style.color='yellow';
@@ -26,16 +21,33 @@ for (var i=0;i<labs.length;i++){
 
 table_style();
 guess_img_style();
+my_picture_style();
+
+function my_picture_style(){
+    var container_picture=document.getElementById('container_picture');
+    container_picture.style.display='inline-block';
+
+  //  container_picture.style.border="2px solid red";
+    container_picture.style.margin="0px 0px 0px 550px";
+
+
+}
+
 
 function guess_img_style(){
 
   var div_img=document.getElementById('img_guess');
+  var div_answer=document.getElementById('div_answer');
+
+    div_answer.style.display="inline-block";
+    div_answer.style.marginLeft="550px";
+    div_answer.style.border="3px solid yellow";
+
   div_img.style.border="3px solid yellow";
   div_img.style.width="300px";
   div_img.style.backgroundColor='black';
-// div_img.style.height="300px";
   div_img.style.display='inline-block';
-    div_img.style.margin='0 0 0 35%';
+   div_img.style.marginLeft='50px';
 }
 
 function table_style() {
@@ -46,11 +58,12 @@ for(var i=0; i<my_td.length;i++){
 
     my_td[i].style.width="100px";
     my_td[i].style.height="120px";
-    my_td[i].style.marginRight="auto";
-    my_td[i].style.marginLeft="auto";
+  //  my_td[i].style.marginRight="auto";
+  //  my_td[i].style.marginLeft="auto";
     my_td[i].style.border="1px solid black";
-    my_td[i].style.backgroundImage="url(static/img/dos.jpg)";
+    my_td[i].style.backgroundImage="url(static/img/dos.png)";
     my_td[i].style.backgroundPosition="center";
+    my_td[i].style.backgroundColor="yellow";
     my_td[i].style.backgroundRepeat="no-repeat";
 
 
