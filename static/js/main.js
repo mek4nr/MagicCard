b753f5bf877090c233adc19f485046851fffe043
mek4nr <jbaptiste.munieres@gmail.com>
2017-05-16 12:33:50 -0400

    dqzzd

--- static/js/main.js
+++ static/js/main.js
@@ -8,7 +8,7 @@ function chrono(){
     centi++; //incrémentation des dixièmes de 1
     if (centi>9){centi=0;secon++} //si les dixièmes > 9,on les réinitialise à 0 et on incrémente les secondes de 1
     if (secon>59){secon=0;minu++} //si les secondes > 59,on les réinitialise à 0 et on incrémente les minutes de 1
-    document.getElementById('sp_time').textContent= minu + ":" + secon +":"+ centi;
+    document.getElementById('current_time').textContent= minu + ":" + secon +":"+ centi;
     let compte=setTimeout('chrono()',100) ;//la fonction est relancée tous les 10° de secondes
 }
 
